57e4dcd8a984729924b80cb5e2ecf5701bde77dc
Ellie Schaefer <111202585+flowersin@users.noreply.github.com>
2025-03-05 17:19:13 -0700

    Created main game files

---
+++ GameEditor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Silly Little RPG Engine Editor
+
+def main(): # Define the main function
+    print("Welcome to the Silly Little RPG Engine Game Editor!") # Explain the program
+
+if __name__ == "__main__": # If this is the main module...
+    main() # Run the main function
---
+++ GamePlayer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Silly Little RPG Engine Player
+
+def main(): # Define the main function
+    print("Welcome to the Silly Little RPG Engine Game Player!") # Explain the program
+
+if __name__ == "__main__": # If this is the main module...
+    main() # Run the main function
